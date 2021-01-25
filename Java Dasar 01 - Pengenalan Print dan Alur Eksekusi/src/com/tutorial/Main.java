93596552d6c43e083ea542433d43eea3fddab13a
Sandy Rahman <tetsuraka@gmail.com>
2021-01-25 09:38:57 +0700

    Push Project 02

---
+++ Java Dasar 01 - Pengenalan Print dan Alur Eksekusi/src/com/tutorial/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tutorial;
+
+/**
+ *
+ * @author IT010318
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //Print data satu baris
+        System.out.println("Baris 1 : Hello bro");
+        System.out.println("Baris 2 : Hello juga bro");
+        
+        //Print data dengan meneruskan
+        System.out.print("Baris 3 : Ini baris ke tiga");
+        System.out.print("baris 4 : Saya meneruskan baris ke tiga");
+        
+        //Print data campur
+        System.out.print("Baris 5 : Hallo bro bro");
+        System.out.println("Baris 6 : Hallo juga brocul!!");
+        System.out.print("Baris 7 : Yuk kongkow lah \n");
+        System.out.print("Baris 8 : Kongkow kemana bro ?? \n");
+        System.out.print("baris 9 : Ke bromo yuk \n");
+        
+        //Print Format data
+        System.out.printf("Wiro sambleng naga geni %d \n", 212);
+    }
+    
+}
---
+++ Java Dasar 02 - Variabel, Assignment dan Deklarasi/src/com/tutorial/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tutorial;
+
+/**
+ *
+ * @author IT010318
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+        // ini adalah komen
+        // Kalau multine dibawah ini
+        /*
+        abcd
+        abcd
+        abcd
+        */
+        
+        //Mmembuat variable, contoh x
+        //Tipe data, contoh int
+        //Assigment, contoh nilai 10
+        int x = 10;
+        System.out.println("Nilai x = " + x);
+        
+        x = 20;
+        System.out.println("Nilai x = " + x);
+        
+        //Membuat Deklarasi
+        
+    }
+    
+}
